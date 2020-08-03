1019fd75dd5839fabcca65611c95fcd4e8432bf4
veenishkoul <veenishkoul@gmail.com>
2020-08-03 19:55:00 -0400

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,12 @@
+""""
+print("Enter first number :")
+inpnum1 = input();
+print("Enter second number :")
+inpnum2 = input();
+print("Sum of two numbers :", int(inpnum1) + int(inpnum2))
+mystr = "i am Samridhee"
+print(mystr)
+#print(mystr.replace("i","are",1))
+print(mystr[0:2])
+"""
+
