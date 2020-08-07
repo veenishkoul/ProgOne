089c1a8835e925a1230601370efec551c48a8f91
veenishkoul <veenishkoul@gmail.com>
2020-08-06 22:20:39 -0400

    Exmple1

---
+++ Test2.py
@@ -0,0 +1,14 @@
+
+dict = {'set':'collection of well define objects',
+        'mutable':'something that can be changed',
+        'immutable': 'something that cannot be changed',
+        'benefit' : 'advantage',
+        'dally':'waste time'
+        }
+print("Welcome to dictionary")
+print("input any word to know the meaning :")
+InputValue = input()
+print("Meaning of user input word is: ",dict[InputValue])
+
+
+
