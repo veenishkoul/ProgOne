ea67d070d1275a7ada60686d54efa231b7d34160
veenishkoul <veenishkoul@gmail.com>
2020-08-06 22:21:26 -0400

    Prep

--- Test1.py
+++ Test1.py
@@ -1,4 +1,4 @@
-""""
+
 print("Enter first number :")
 inpnum1 = input();
 print("Enter second number :")
@@ -8,5 +8,23 @@ mystr = "i am Samridhee"
 print(mystr)
 #print(mystr.replace("i","are",1))
 print(mystr[0:2])
-"""
+
+
+Mylist = ['veenish',23,'ball','cat']
+Mylist.reverse()
+print(Mylist)
+
+Mytup = ('Hello',35,'Wheel','Bat')
+print(Mytup)
+
+
+a = 1
+b = 3
+a,b = b,a
+print(a)
+print(b)
+
+Mylist2 = [1,2,3,4,11,56,22,'goat','hat',33]
+Mytup2 = tuple(Mylist2)
+print(Mytup2)
 
