67cd50a4d42752528008d69a992576cf2d128b26
veenishkoul <veenishkoul@gmail.com>
2020-08-08 20:04:05 -0400

    Test3

---
+++ Test3.py
@@ -0,0 +1,22 @@
+#This script checks for the leap year
+
+
+def is_leap(is_leap_year):
+    leap = False
+    if is_leap_year % 4 == 0:
+        if is_leap_year % 100 == 0:
+            if is_leap_year % 400 == 0:
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+    return leap
+
+
+print("Enter the year:")
+raw_year = input()
+is_leap_year = int(raw_year)
+print(is_leap(is_leap_year))
