01f9ffa87be02db66b1743d80e0d012d6c67458c
veenishkoul <veenishkoul@gmail.com>
2020-08-10 17:21:56 -0400

    Test4

--- Test4.py
+++ Test4.py
@@ -1,10 +1,16 @@
 # python script to print string of number in series
 
-var1 = ''
-print("Enter any interger :")
-n = int(input())
+# var1 = ''
+# print("Enter any interger :")
+# n = int(input())
+# for x in range(1,n+1):
+#     var1 = str(var1) + str(x)
+# print(var1)
 
-for x in range(1,n+1):
-    var1 = str(var1) + str(x)
-print(var1)
+
+list1 = [2,6,3]
+print(5  not in list1)
+if(5 not in list1):
+    print("Not in list")
+
 
---
+++ Test5.py
@@ -0,0 +1,2 @@
+for var in (sorted([5, 2, 3, 1, 4])):
+    print(var)
