78c534fead8160557ed218bef2fdf729467bfd9a
veenishkoul <veenishkoul@gmail.com>
2020-08-11 20:41:02 -0400

    Test4

--- Test4.py
+++ Test4.py
@@ -1,16 +1,29 @@
 # python script to print string of number in series
 
-# var1 = ''
-# print("Enter any interger :")
-# n = int(input())
-# for x in range(1,n+1):
-#     var1 = str(var1) + str(x)
-# print(var1)
+var1 = ''
+print("Enter any interger :")
+n = int(input())
+for x in range(1,n+1):
+    var1 = str(var1) + str(x)
+print(var1)
 
-
+###########To check list logic##########
 list1 = [2,6,3]
 print(5  not in list1)
 if(5 not in list1):
     print("Not in list")
 
+#####TO test age for driving########
+
+print("what is your age")
+age = int(input())
+if (age > 7 and age < 100):
+    if age < 18:
+        print("you cannot drive")
+    elif age == 18:
+        print("we will see")
+    else:
+        print("you can drive")
+else:
+    print("Enter valid age")
 
